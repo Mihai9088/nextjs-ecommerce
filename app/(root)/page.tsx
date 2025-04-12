94ea76e84ef39cd4d986c93536e50245f3ff9aed
Mihai9088 <mihaitza1058@gmail.com>
2025-04-12 14:55:02 +0200

    added sample products and product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 
 const HomePage = async () => {
-
   return (
-    <div>
-      Page
-    </div>
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
   );
 };
 
