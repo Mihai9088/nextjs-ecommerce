e094310781fdbf769cbdaecef097304cecd60036
Mihai Voina <mihaitza1058@gmail.com>
2025-04-16 15:36:13 +0300

    fixed the not found page layout on mobile

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -14,12 +14,12 @@ const NotFoundPage = () => {
         alt={`${APP_NAME} logo`}
         priority={true}
       />
-      <div className="p-6 w-1/3 rounded-lg shadow-md text-center">
+      <div className="p-6  rounded-lg shadow-md w-full max-w-md text-center">
         <h1 className="text-3xl font-bold mb-4">Not Found</h1>
         <p className="text-destructive">Could not find requested page</p>
         <Button
           variant="outline"
-          className="mt-4 ml-2"
+          className="mt-4 ml-2 cursor-pointer"
           onClick={() => (window.location.href = "/")}
         >
           Back Home
