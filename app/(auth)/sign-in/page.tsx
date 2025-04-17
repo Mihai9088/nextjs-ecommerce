a30ebc6ea9f591ca01d1cc7138a1d765fe50f4d3
Mihai Voina <mihaitza1058@gmail.com>
2025-04-17 19:03:54 +0300

    added authenticated status and sign in form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -10,12 +10,27 @@ import Link from "next/link";
 import React from "react";
 import Image from "next/image";
 import { APP_NAME } from "@/lib/constants";
+import CredentialsSignInForm from "./credentials-signin-form";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Sign In",
 };
 
-const SignInPage = () => {
+const SignInPage = async (props: {
+  searchParams: Promise<{
+    callbackUrl: string;
+  }>;
+}) => {
+  const { callbackUrl } = await props.searchParams;
+
+  const session = await auth();
+
+  if (session) {
+    return redirect(callbackUrl || "/");
+  }
+
   return (
     <div className="w-full max-w-md mx-auto">
       <Card>
@@ -34,7 +49,9 @@ const SignInPage = () => {
             Sign in to your account
           </CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4">{/* Sign In Form  */}</CardContent>
+        <CardContent className="space-y-4">
+          <CredentialsSignInForm />
+        </CardContent>
       </Card>
     </div>
   );
--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import "./loading.css";
 
 const LoadingPage = () => {
   return (
-    <div className="flex justify-center items-center h-screen w-screen bg-gray-100 dark:bg-gray-900">
+    <div className="flex justify-center items-center h-screen w-screen">
       <div className="spinner"></div>
     </div>
   );
