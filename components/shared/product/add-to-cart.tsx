22101d79e50d802ec6e497407aa9648b2d31ab61
Mihai Voina <mihaitza1058@gmail.com>
2025-04-21 16:29:33 +0300

    finished cart functionality

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,43 +1,107 @@
 "use client";
 import React from "react";
-import { CartItem } from "@/types";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus } from "lucide-react";
-import { addItemToCart } from "@/lib/actions/cart.actions";
 import { toast } from "sonner";
+import { Cart, CartItem } from "@/types";
+import { Plus, Minus, Loader } from "lucide-react";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { useTransition } from "react";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ item, cart }: { item: CartItem; cart?: Cart }) => {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      const res = await addItemToCart(item);
 
-    if (!res.success) {
-      toast.error(res.message);
-      return;
-    }
+      if (!res.success) {
+        toast.error(res.message);
+        return;
+      }
 
-    toast(res.message, {
-      action: (
-        <Button
-          className="bg-primary cursor-pointer text-white hover:bg-gray-800"
-          onClick={() => router.push("/cart")}
-        >
-          Go to cart
-        </Button>
-      ),
+      toast(res.message, {
+        action: (
+          <Button
+            className="bg-primary cursor-pointer"
+            onClick={() => router.push("/cart")}
+          >
+            Go to cart
+          </Button>
+        ),
+      });
     });
   };
 
-  return (
+  const handleRemoveFromCart = async () => {
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
+
+      if (!res.success) {
+        toast.error(res.message);
+        return;
+      }
+
+      toast(res.message, {
+        action: (
+          <Button
+            className="bg-primary  cursor-pointer"
+            onClick={() => router.push("/cart")}
+          >
+            Go to cart
+          </Button>
+        ),
+      });
+    });
+  };
+
+  const existItem =
+    cart && cart.items.find((x) => x.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button
+        className="cursor-pointer"
+        type="button"
+        variant="outline"
+        disabled={isPending}
+        onClick={handleRemoveFromCart}
+      >
+        {isPending ? (
+          <Loader className="w-4 h-4  animate-spin" />
+        ) : (
+          <Minus className="w-4 h-4" />
+        )}
+      </Button>
+      <span className="px-2">{existItem.qty}</span>
+      <Button
+        className="cursor-pointer"
+        type="button"
+        variant="outline"
+        disabled={isPending}
+        onClick={handleAddToCart}
+      >
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="w-4 h-4" />
+        )}
+      </Button>
+    </div>
+  ) : (
     <Button
       className="w-full cursor-pointer"
       type="button"
+      disabled={isPending}
       onClick={handleAddToCart}
     >
-      <Plus />
-      AddToCart
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <Plus className="w-4 h-4" />
+      )}
+      Add to cart
     </Button>
   );
 };
