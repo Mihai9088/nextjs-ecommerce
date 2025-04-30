a0bd6004918da22ac790cfb531556cdca83d9627
Mihai Voina <mihaitza1058@gmail.com>
2025-04-30 14:32:21 +0300

    finished user profile section

--- lib/validators.ts
+++ lib/validators.ts
@@ -107,3 +107,8 @@ export const paymentResultSchema = z.object({
   pricePaid: z.string(),
   email_address: z.string(),
 });
+
+export const updateProfileSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters"),
+  email: z.string().min(3, "Email must be at least 3 characters"),
+});
