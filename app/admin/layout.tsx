12d1d43b6ae471330ee65ca32edc3302b5544f6b
Mihai Voina <mihaitza1058@gmail.com>
2025-05-07 15:56:30 +0300

    added users and search features

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Menu from "@/components/shared/header/menu";
 import MainNav from "./main-nav";
-import { Input } from "@/components/ui/input";
+import AdminSearch from "@/components/shared/admin/admin-search";
 
 export default function AdminLayout({
   children,
@@ -26,13 +26,11 @@ export default function AdminLayout({
             <MainNav className="mx-6" />
             <div className="ml-auto flex items-center space-x-4">
               <div>
-                <Input
-                  type="search"
-                  placeholder="Search..."
-                  className="md:w-[100px] lg:w-[300px]"
-                />
+                <div className="ml-auto flex items-center space-x-4">
+                  <AdminSearch />
+                  <Menu />
+                </div>
               </div>
-              <Menu />
             </div>
           </div>
         </div>
